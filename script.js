aa111c7fd93f8662628783b058402391d7b77aa5
Cristian Brochero <cistianbrocheropacheco@gmail.com>
2021-05-01 10:09:42 -0500

    Comentarios, Sentencias, Variables, Constantes y uso de let

---
+++ script.js
@@ -0,0 +1,46 @@
+alert ('¡Soy JavaScript!');
+
+//comentarios
+/*de mas de una
+linea*/
+
+// sentencias
+
+/* alert('Hola');
+alert('mundo');*/
+
+
+
+let a = 5;
+let b = 6;
+let suma =a + b;
+console.log ("El resultado de la suma es: ", suma);
+b=7;
+console.log(b);
+
+let nombre = "Cristian";
+let nombre2 = nombre;
+
+nombre2 = "carlos";
+console.log("el nombre uno es ", nombre);
+console.log("el nombre dos es ", nombre2);
+
+// variables con camel case
+nombreUno = "Cristian";
+nombreDos = "carlos";
+
+//nombrado underscore
+nombre_uno = "Cristian";
+nombre_dos = "carlos";
+
+//constantes
+
+const pi = 3.141624;
+const COLOR_ROJO ="#FFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi);
+
+const id_account = get_id_account(45);
+
